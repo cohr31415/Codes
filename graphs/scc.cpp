086bb6929b9a36b209de926a5a32be94367f8bc1
cohr31415 <48498792+cohr31415@users.noreply.github.com>
2020-02-02 23:24:25 -0500

    Update scc.cpp

--- graphs/scc.cpp
+++ graphs/scc.cpp
@@ -40,9 +40,11 @@ void __f(const char* names, Arg1&& arg1, Args&&... args) {
 struct SCC{
     vector <vector<int>> adj, adj_i;
     vector <bool> used;
-    vector <int> order, component;
+    vector <int> order, component, color;
     // se usa para inicializar los grafos con n vértices y m aristas
     void init(int n, int m){
+        adj.clear(); adj_i.clear(); order.clear();
+        color.assign(n + 1, 0);
         vector <int> zero; zero.clear();
         for(int i = 0; i <= n; i++){
             adj.push_back(zero);
@@ -64,12 +66,30 @@ struct SCC{
     // segundo dfs, se tiene que recorrer de reversa, y hacer dfs
     // y todos los que salen son componente, que se guarda en component
     // notar que en cada iteración, component se tiene que limpiar
-    void dfs2(int u){
+    void dfs2(int u, int col){
         used[u] = true;
         component.push_back(u);
+        color[u] = col;
         for(auto x: adj_i[u]){
-            if(!used[x]) dfs2(x);
+            if(!used[x]) dfs2(x, col);
         }
+    }
+    //resuelve el scc y en color te guarda el color en el que guarda
+    int go(int n, int m){
+        for(int i = 1; i <= n; i++){
+            if(!used[i]) dfs1(i);
+        }
+        limpiar(n, m);
+        int col = 1;
+        for(int i = 0; i < n; i++){
+            int u = order[n - i - 1];
+            if(!used[u]) {
+                dfs2(u, col);
+                col++; 
+            }
+        }
+        //retorna la cantidad de colores 
+        return col - 1;
     }
 } F;
  
@@ -78,11 +98,21 @@ int main(){
     int n, m;
     cin >> n >> m;
     //se leen las aristas
+    F.init(n, m);
+    F.limpiar(n, m);
     for(int i = 0; i < m; i++){
         int a, b; cin >> a >> b;
         F.adj[a].push_back(b);
         F.adj_i[b].push_back(a);
+        //aristas.push_back({a, b});
     }
+    int n2 = F.go(n, m);
+    for(auto x: aristas){
+        int a = F.color[x.ff], b = F.color[x.ss];
+        if(a == b) continue;
+        //trace(a, b);
+    }
+    for(int i = 1; i <= n2; i++) {sort(xd[i].begin(), xd[i].end()); xd[i].erase(unique(xd[i].begin(),xd[i].end()),xd[i].end());}
     return 0;
  
 }
