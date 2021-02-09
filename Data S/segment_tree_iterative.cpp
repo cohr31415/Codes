0e2b50b0adc855729d8663abeac6f99e5c20e99c
danielcoronel314 <48498792+danielcoronel314@users.noreply.github.com>
2021-02-09 11:42:24 -0500

    Update segment_tree_iterative.cpp

--- Data S/segment_tree_iterative.cpp
+++ Data S/segment_tree_iterative.cpp
@@ -1,15 +1,17 @@
+//se inicializa con init(arreglo)
+//se modifica p = pos, v = val
 int st[2 * N];
 int a[N];
 int n;
 
 inline int op(int a, int b){ return a + b;}
 
-void init(int* a){
-    for(int i = 0; i < n; i++) st[i + n] = a[i];
+void init(int* a){ //inicia
+    for(int i = 0; i < n; i++) st[i + n] = a[i]; //arreglo original
     for(int i = n - 1; i > 0; i--) st[i] = op(st[i << 1], st[(i << 1) | 1]);
 }
 
-void update(int p, int v){
+void update(int p, int v){ //modifica
     st[p += n] = v;
     for(p; p > 1; p >>= 1) st[p >> 1] = op(st[p], st[p^1]);
 }
@@ -20,5 +22,5 @@ int query(int l, int r){ //query de [l, r)
         if(l & 1) ans = op(ans, st[l++]);
         if(r & 1) ans = op(ans, st[--r]);
     }
-    return ans;
+    return ans; //rspuesta
 }
